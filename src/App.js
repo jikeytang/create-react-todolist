3e03f69b3d5bcd0437375bb06e7d09a0c286089e
jikeytang <jikeytang@gmail.com>
2019-05-15 22:43:53 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import './styles/App.css'
 import TodoApp from './pages'
-// import { createStore } from 'redux'
-// import { Provider } from 'react-redux'
-// import rootReducer from './reducers'
-
-// const store = createStore(rootReducer)
 
 function App () {
   return (
-    // <Provider store={store}>
       <TodoApp />
-    // </Provider>
   )
 }
 
